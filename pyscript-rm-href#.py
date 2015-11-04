44e64f6be59dc32d91e2fc5f835ec16468d77980
Jasmin George <jasmingeorge21@gmail.com>
2015-11-04 20:26:29 +0530

    updated script to remove href attribute

--- pyscript-rm-href#.py
+++ pyscript-rm-href#.py
@@ -1,27 +1,27 @@
-import re 
+import re
 import os
 import fnmatch
 
 config_path = raw_input("Enter full path : ")
 log = open(config_path + '/log.txt', 'w')
 
-def replace_href(file):
-	log.write(file + '\n')
-	txt = open(file)
-	lines = txt.readlines()
-	for idx, line in enumerate(lines):
-		if len(line) == 0:
-			continue
-		if re.match(".*<a.*", line):
-			log.write(line)
-			if ' href="#"' in line:
-				line = line.replace(' href="#"', "")
-				log.write("-->" + line) 
-		lines[idx] = line
-	out = open(file, 'w')
-	lines = ''.join(lines)
-	out.write(lines)
 
-for root, dirnames, file in os.walk(config_path):
-	for filename in fnmatch.filter(file, '*.html'):
-		replace_href(os.path.join(root, filename))
+def replace_href(file_name):
+    log.write(file_name + '\n')
+    txt = open(file_name)
+    lines = txt.readlines()
+    for idx, line in enumerate(lines):
+        if len(line) == 0:
+            continue
+        if ' href="' in line:
+            log.write(line)
+            line = re.sub(r' href="#?/?[a-z/]*"', "", line)
+            log.write("-->" + line)
+        lines[idx] = line
+    out = open(file_name, 'w')
+    lines = ''.join(lines)
+    out.write(lines)
+
+for root, dir_names, file in os.walk(config_path):
+    for filename in fnmatch.filter(file, '*.html'):
+        replace_href(os.path.join(root, filename))
